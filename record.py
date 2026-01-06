830c0d8090dee5fc99d2894cd3d5380042d1b05d
mphele <mphelemoswane3@gmail.com>
2026-01-06 16:49:24 +0200

    File saving implemented

--- record.py
+++ record.py
@@ -1,38 +1,46 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 import wavio as wv 
-
-
+import time
+import sys
+import numpy as np
+import os
+from pathlib import Path
 
 sampling_frequency = 48000
+filename = input("What do you wish to name the recording file?: ")
 recording_duration = float(input("How many seconds do you wish to record for?: "))
 channels = 1
 
-print("Recording has begun please begin speaking into mic")
-print(f"Recording time remaining: ")
-recording = sd.rec(int(recording_duration*sampling_frequency), samplerate=sampling_frequency, channels=channels)
-
-sd.wait()
-
-import time
-import sys
-
 def countdown(t):
-    while t >= 0:
-        # Convert total seconds into minutes and seconds
-        mins, secs = divmod(t, 60)
-        # Format the time string with leading zeros (e.g., 05:02)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
+    while t > 0:
+        secs = int(t)
+        timer = f'{secs:02d}'
         
-        # Print the time on the same line, overwriting the previous output
-        sys.stdout.write('\rTime left: ' + timer)
-        sys.stdout.flush() # Forces the output to be displayed immediately
         
-        # Pause the loop for 1 second
-        time.sleep(1)
-        
-        # Decrease the time by 1 second
+        sys.stdout.write('\r' + "Recording time remaining: "+ timer)
+        sys.stdout.flush()
+        time.sleep(1) 
         t -= 1
-    
-    # After the loop finishes (t reaches 0), print a final message on a new line
-    print('\nTimes up!!!')
+
+
+print("Recording has begun please begin speaking into mic")
+recording = sd.rec(int(recording_duration*sampling_frequency), samplerate=sampling_frequency, channels=channels)
+countdown(recording_duration)
+sd.wait()
+print()
+print("Recording finished.")
+
+def find_file_anywhere(filename, search_path='/'):
+    for root, dirs, files in os.walk(search_path):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+wv.write(f"{filename}.wav", recording, sampling_frequency,sampwidth=2)
+file_to_find = f"{filename}.wav"
+found_location = find_file_anywhere(file_to_find, search_path= os.getcwd()) 
+
+print(f"Audio file saved as {filename}.wav at {found_location}")
+
+
